7316198d1ed1be458e198b8c3bc5b53e3fa34732
rkorostin <rkorostin@gmail.com>
2023-02-17 13:52:21 +0300

    add method getNumberNyUser

--- hw_02/task_power.java
+++ hw_02/task_power.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class task_power {
     public static void main(String[] args) {
         System.out.println(power(5, 4));
@@ -9,5 +11,20 @@ public class task_power {
             result *= value;
         }
         return result;
+    }
+
+    public static int getNumberByUser(String text) {
+        int i;
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print(text);
+            if (sc.hasNextInt()) {
+                i = sc.nextInt();
+                break;
+            } else {
+                System.out.println("Вы ввели не целое число");
+            }
+        }
+        return i;
     }
 }
